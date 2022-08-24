b4ca45eaddf85c04b07cfe0fc080999036d793f6
Bonboncy99 <thaonhien9991@gmail.com>
2022-08-24 19:50:07 +0700

    ss7 spring data repository

---
+++ ss7_spring_data_repository/exercise/blog-management-beautiful/src/main/java/com/codegym/service/IBlogService.java
@@ -0,0 +1,18 @@
+package com.codegym.service;
+
+import com.codegym.model.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface IBlogService {
+    Page<Blog> findAll(Pageable pageable);
+    void addBlog(Blog blog);
+    void updateBlog(Blog blog);
+    void deleteBlog(int id);
+    Optional<Blog> findById(int id);
+    Page<Blog>findAllByBlogNameContainingAndCategories_Name(String blogName, String categoryName, Pageable pageable);
+}
---
+++ ss7_spring_data_repository/exercise/blog-management-beautiful/src/main/java/com/codegym/service/ICategoryService.java
@@ -0,0 +1,19 @@
+package com.codegym.service;
+
+import com.codegym.model.Blog;
+import com.codegym.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICategoryService {
+    List<Category> findAll();
+    void add(Category category);
+    void update(Category category);
+    void delete(int id);
+    Optional<Blog> findById(int id);
+
+}
