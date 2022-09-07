1d058b18924a9e2f070a0c1ec68ae6e2fe61d7dc
Bonboncy99 <thaonhien9991@gmail.com>
2022-09-07 16:20:00 +0700

    ss13 Security

--- ss13_security/exercise/blog-management-beautiful_13/src/main/java/com/codegym/config/WebSecurityConfig.java
+++ ss13_security/exercise/blog-management-beautiful_13/src/main/java/com/codegym/config/WebSecurityConfig.java
@@ -53,10 +53,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // Trang /userInfo yêu cầu phải login với vai trò ROLE_USER hoặc ROLE_ADMIN.
         // Nếu chưa login, nó sẽ redirect tới trang /login.
-        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+        http.authorizeRequests().antMatchers("/userInfo","/blog/showCreate").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
 
         // Trang chỉ dành cho ADMIN
-        http.authorizeRequests().antMatchers("/admin","/blog/showCreate","/blog/showUpdate","/blog/delete").access("hasRole('ROLE_ADMIN')");
+        http.authorizeRequests().antMatchers("/admin","/blog/showUpdate","/blog/delete").access("hasRole('ROLE_ADMIN')");
 
         // Khi người dùng đã login, với vai trò XX.
         // Nhưng truy cập vào trang yêu cầu vai trò YY,
--- ss13_security/exercise/blog-management-beautiful_13/src/main/java/com/codegym/controller/BlogController.java
+++ ss13_security/exercise/blog-management-beautiful_13/src/main/java/com/codegym/controller/BlogController.java
@@ -3,15 +3,25 @@ package com.codegym.controller;
 import com.codegym.model.Blog;
 import com.codegym.service.IBlogService;
 import com.codegym.service.ICategoryService;
+import com.codegym.util.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.management.relation.Role;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @RequestMapping("/blog")
@@ -23,53 +33,87 @@ public class BlogController {
     private ICategoryService iCategoryService;
 
     @GetMapping("/showCreate")
-    public String goToAdd(Model model){
-        model.addAttribute("blog",new Blog());
-        model.addAttribute("categoryList",this.iCategoryService.findAll());
+    public String goToAdd(Model model) {
+        model.addAttribute("blog", new Blog());
+        model.addAttribute("categoryList", this.iCategoryService.findAll());
         return "create";
     }
 
     @PostMapping("/create")
-    public String create (@ModelAttribute Blog blog, RedirectAttributes redirectAttributes){
+    public String create(@ModelAttribute Blog blog, RedirectAttributes redirectAttributes) {
         iBlogService.addBlog(blog);
-        redirectAttributes.addFlashAttribute("messCreate","Create Success");
+        redirectAttributes.addFlashAttribute("messCreate", "Create Success");
         return "redirect:/blog/";
     }
+
     @GetMapping("/showUpdate")
-    public String goToAdd(@RequestParam int id, Model model){
-        model.addAttribute("blog",this.iBlogService.findById(id).get());
-        model.addAttribute("categoryList",this.iCategoryService.findAll());
+    public String goToAdd(@RequestParam int id, Model model) {
+        model.addAttribute("blog", this.iBlogService.findById(id).get());
+        model.addAttribute("categoryList", this.iCategoryService.findAll());
         return "update";
     }
+
     @PostMapping("/update")
-    public String update (@ModelAttribute Blog blog, RedirectAttributes redirectAttributes){
+    public String update(@ModelAttribute Blog blog, RedirectAttributes redirectAttributes) {
         iBlogService.updateBlog(blog);
-        redirectAttributes.addFlashAttribute("messUpdate","Update Success");
+        redirectAttributes.addFlashAttribute("messUpdate", "Update Success");
         return "redirect:/blog/";
     }
-    @PostMapping("/delete")
-    public String goToAdd(@RequestParam int idDelete){
+
+    @GetMapping("/delete")
+    public String goToAdd(@RequestParam int idDelete) {
         iBlogService.deleteBlog(idDelete);
         return "redirect:/blog/";
     }
+
     @GetMapping("detail")
-    public String goToDetail(@RequestParam int id, Model model){
-        model.addAttribute("blog",this.iBlogService.findById(id).get());
+    public String goToDetail(@RequestParam int id, Model model) {
+        model.addAttribute("blog", this.iBlogService.findById(id).get());
         return "detail";
     }
 
-    @GetMapping(value = {"/search","/"})
-    public String search(@RequestParam Optional <Integer> page,
-                         @RequestParam(required = false,defaultValue = "") String category,
-                         @RequestParam(required = false,defaultValue = "") String nameSearch,
-                         @PageableDefault(size = 7, sort = "id",direction = Sort.Direction.DESC) Pageable pageable, Model model){
-        Page<Blog> blogList =this.iBlogService.findAllByBlogNameContainingAndCategories_Name(nameSearch,category,pageable);
-        model.addAttribute("categoryList",this.iCategoryService.findAll());
-        model.addAttribute("nameSearch",nameSearch);
-        model.addAttribute("category",category);
-        model.addAttribute("blogList",blogList);
-        model.addAttribute("size",pageable.getPageSize());
+    @GetMapping(value = {"/search", "/"})
+    public String search(Principal principal,
+                         @RequestParam Optional<Integer> page,
+                         @RequestParam(required = false, defaultValue = "") String category,
+                         @RequestParam(required = false, defaultValue = "") String nameSearch,
+                         @PageableDefault(size = 7, sort = "id", direction = Sort.Direction.DESC) Pageable pageable, Model model) {
+        Page<Blog> blogList = this.iBlogService.findAllByBlogNameContainingAndCategories_Name(nameSearch, category, pageable);
+
+        User loginedUser = (User) ((Authentication) principal).getPrincipal();
+        String userInfo = WebUtils.toString(loginedUser);
+        model.addAttribute("userInfo", userInfo);
+
+//        List<GrantedAuthority> grantedAuthorities = new ArrayList<>(loginedUser.getAuthorities());
+//        String userRole;
+//        for (GrantedAuthority grantedAuthority : grantedAuthorities) {
+//            if (grantedAuthority.equals("ROLE_ADMIN")) {
+//                userRole = "admin";
+//            } else if (grantedAuthority.equals("ROLE_USER")) {
+//                userRole = "user";
+//            }
+//        }
+//        model.addAttribute("userRole",userRole);
+
+
+
+//        Collection<GrantedAuthority> grantedAuthorityList = loginedUser.getAuthorities();
+//        if (grantedAuthorityList.size() == 2) {
+//            model.addAttribute("admin", "admin");
+//        } else if (grantedAuthorityList.size() == 1) {
+//            model.addAttribute("user", "user");
+//        } else {
+//            model.addAttribute("notUser", "notUser");
+//        }
+
+
+
+
+        model.addAttribute("categoryList", this.iCategoryService.findAll());
+        model.addAttribute("nameSearch", nameSearch);
+        model.addAttribute("category", category);
+        model.addAttribute("blogList", blogList);
+        model.addAttribute("size", pageable.getPageSize());
         return "list";
     }
-
 }
